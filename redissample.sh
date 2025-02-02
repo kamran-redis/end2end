89bd6d6dcf1853822998480488109923fb8e4bb7
Kamran Yousaf <kamran-redis@users.noreply.github.com>
2025-02-02 18:38:32 +0000

    added sample queries

--- redissample.sh
+++ redissample.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #./redissample.sh --redis.host redis --jobname helloworld --parallelism 4 --ratelimit 4000
 #rate(flink_taskmanager_job_task_operator_helloworld_MY2eventCounter[1m])
+#sum(rate(flink_taskmanager_job_task_operator_helloworld_MY2eventCounter[1m]))
 #flink_taskmanager_job_task_operator_helloworld_MY1eventTimeLag{quantile="0.99"}
+#avg(flink_taskmanager_job_task_operator_helloworld_MY1eventTimeLag{quantile="0.99"})
 ./gradlew clean shadowJar
 
 docker cp ./build/libs/end2end-all-1.0-SNAPSHOT.jar jobmanager:/opt/flink/examples/streaming
