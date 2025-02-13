4fc14f33395e1f04bba343d54284b33e2aa8cf2f
Kamran Yousaf <kamran-redis@users.noreply.github.com>
2025-02-13 12:33:19 +0000

    commented openconsoles

--- openconsoles.sh
+++ openconsoles.sh
@@ -1,5 +1,10 @@
-open -a "Google Chrome" http://localhost:8001 
-open -a "Google Chrome" http://localhost:8081 
+# redis insight
+open -a "Google Chrome" http://localhost:8001
+#flink dashboard
+open -a "Google Chrome" http://localhost:8081
+#mongoexpress
 open -a "Google Chrome" http://localhost:8082
+# prometheus
 open -a "Google Chrome" http://localhost:9090
+# kafkaui
 open -a "Google Chrome" http://localhost:8080
